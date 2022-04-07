0527537492a675b216713c626c216ab200a0f1dc
mahakaalboyzz <103128215+mahakaalboyzz@users.noreply.github.com>
2022-04-07 10:46:58 +0530

    Add files via upload

---
+++ Findgreater.java
@@ -0,0 +1,26 @@
+
+
+public class Findgreater
+   {
+      public static void main(String args[])
+        {
+          int Num1=100;
+          int Num2=324;
+          int Num3=333;
+          
+          if(Num1>Num2)
+             {
+                if(Num1>Num3)
+                     System.out.println(Num1);
+                else
+                     System.out.println(Num3);
+             }
+          else
+             if(Num2>Num3)
+                     System.out.println(Num2);
+                else
+                     System.out.println(Num3);
+         }
+}
+ 
+    
